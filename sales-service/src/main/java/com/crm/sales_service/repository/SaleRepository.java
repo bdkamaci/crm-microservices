bcad56a2fbf9488aa9a8dfbd5f9e475b7d2cc550
bdkamaci <burcudogakamaci@gmail.com>
2025-02-05 21:32:18 +0100

    feat: add customised find methods to SaleRepository

--- sales-service/src/main/java/com/crm/sales_service/repository/SaleRepository.java
+++ sales-service/src/main/java/com/crm/sales_service/repository/SaleRepository.java
@@ -1,7 +1,13 @@
 package com.crm.sales_service.repository;
 
 import com.crm.sales_service.model.Sale;
+import com.crm.sales_service.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    List<Sale> findByCustomerId(Long customerId);
+    List<Sale> findByStatus(Status status);
+    List<Sale> findByCustomerIdAndStatus(Long customerId, Status status);
 }
