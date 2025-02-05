50da48643c14dc12d48077a52950ef8e332c5172
bdkamaci <burcudogakamaci@gmail.com>
2025-02-05 21:29:51 +0100

    feat: add customised find methods to CustomerRepository

--- customer-service/src/main/java/com/crm/customer_service/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/crm/customer_service/repository/CustomerRepository.java
@@ -3,5 +3,10 @@ package com.crm.customer_service.repository;
 import com.crm.customer_service.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+    List<Customer> findByCompany(String company);
 }
