ad98ac2338b0f2f8b7d044ab0d1bf97a6e77b656
bdkamaci <burcudogakamaci@gmail.com>
2025-02-05 21:30:27 +0100

    feat: add method to find note by customerId

--- customer-service/src/main/java/com/crm/customer_service/repository/NoteRepository.java
+++ customer-service/src/main/java/com/crm/customer_service/repository/NoteRepository.java
@@ -3,5 +3,8 @@ package com.crm.customer_service.repository;
 import com.crm.customer_service.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByCustomerId(Long customerId);
 }
