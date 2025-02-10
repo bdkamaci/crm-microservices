941c51866b4e70bd628b03354313533270b59ad8
bdkamaci <burcudogakamaci@gmail.com>
2025-02-10 16:43:39 +0100

    chore: modify CustomerRepository to support specification

--- customer-service/src/main/java/com/crm/customer_service/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/crm/customer_service/repository/CustomerRepository.java
@@ -2,11 +2,12 @@ package com.crm.customer_service.repository;
 
 import com.crm.customer_service.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
     Optional<Customer> findByEmail(String email);
     List<Customer> findByCompany(String company);
 }
